c21ee5b749c2c401aa1a57bf3873a1c7afac151b
fatih <qraxiss@gmail.com>
2025-02-19 16:01:58 +0300

    Init

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UsersService = exports.AuthService = exports.OpenAPI = exports.CancelError = exports.CancelablePromise = exports.ApiError = void 0;
 /* generated using openapi-typescript-codegen -- do not edit */
 /* istanbul ignore file */
 /* tslint:disable */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setApiConfig = exports.UsersService = exports.AuthService = exports.OpenAPI = exports.CancelError = exports.CancelablePromise = exports.ApiError = void 0;
 /* eslint-disable */
 var ApiError_1 = require("./core/ApiError");
 Object.defineProperty(exports, "ApiError", { enumerable: true, get: function () { return ApiError_1.ApiError; } });
@@ -16,5 +16,3 @@ var AuthService_1 = require("./services/AuthService");
 Object.defineProperty(exports, "AuthService", { enumerable: true, get: function () { return AuthService_1.AuthService; } });
 var UsersService_1 = require("./services/UsersService");
 Object.defineProperty(exports, "UsersService", { enumerable: true, get: function () { return UsersService_1.UsersService; } });
-var config_1 = require("./config");
-Object.defineProperty(exports, "setApiConfig", { enumerable: true, get: function () { return config_1.setApiConfig; } });
